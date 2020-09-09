18b27832a28035abcb008748dfa6c745dae70db9
Ivan Zanon <ivanzanon@gmail.com>
2020-09-09 16:26:22 -0300

    Minor changes

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,25 +1,24 @@
 'use strict';
 const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    fullname: DataTypes.STRING,
-    password: DataTypes.STRING,
-    level: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+  Model, DataTypes
+} = require('Sequelize');
+
+const sequelize = require('./index');
+
+class User extends Model {};
+
+User.init({
+  username:DataTypes.STRING,
+  fullname: DataTypes.STRING,
+  password: DataTypes.STRING,
+  level: DataTypes.STRING
+}, {
+  sequelize,
+  modelName: 'User',
+});
+
+User.associate = models => {
+  // define association here
+}
+
+module.exports = User;
--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,17 @@
  * 
  */
 
- const express = require('express');
- const authorizationManager = require('./jwt/authorizationManager');
- const routes = express.Router();
+const express = require('express');
+const authorizationManager = require('./jwt/authorizationManager');
+const routes = express.Router();
+const userController = require('./controller/UserController');
 
- //routes.get('/users', userController.index);
+routes.get('/users', userController.index);
+routes.get('/users/:id', authorizationManager.verifyJWT, userController.show);
+routes.post('/users', userController.store);
+routes.put('/users/:id', userController.update);
+routes.delete('/users/:id', userController.destroy);
+routes.post('/login', userController.login);
+routes.post('/userExists', userController.userExists);
 
- module.exports = routes;
+module.exports = routes;
